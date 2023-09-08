786ad4ba01a485246c0a9a10d15a2ceefdd3fbe1
Iago Pinheiro <130023096+iagompinheiro@users.noreply.github.com>
2023-09-08 18:12:57 -0300

    Atualizando método main

--- src/list/operacoesBasicas/CarrinhoCompra.java
+++ src/list/operacoesBasicas/CarrinhoCompra.java
@@ -49,4 +49,21 @@ public class CarrinhoCompra {
                 "itemList=" + itemList +
                 '}';
     }
+
+    public static void main(String[] args) {
+        CarrinhoCompra carrinhoCompra = new CarrinhoCompra();
+
+        carrinhoCompra.adicionarItem("Camiseta", 5d, 1);
+        carrinhoCompra.adicionarItem("Calça", 2d, 1);
+
+        carrinhoCompra.exibirItens();
+        System.out.println("O valor total é: " + carrinhoCompra.calcularValorTotal());
+
+
+        carrinhoCompra.removeItem("Calça");
+        carrinhoCompra.exibirItens();
+
+        System.out.println("O valor total é: " + carrinhoCompra.calcularValorTotal());
+    }
 }
+
