a9e95583de167356b9106f00fb401f5386b9830e
Iago Pinheiro <130023096+iagompinheiro@users.noreply.github.com>
2023-09-13 20:31:47 -0300

    Criação de Collections do tipo SET (operações básicas)

---
+++ src/set/operacoesBasicas/ConjuntoConvidados.java
@@ -0,0 +1,53 @@
+package set.operacoesBasicas;
+
+import java.util.HashSet;
+import java.util.Set;
+
+public class ConjuntoConvidados {
+    private Set<Convidado> convidadoSet;
+
+    public ConjuntoConvidados() {
+        this.convidadoSet = new HashSet<>();
+    }
+
+    public void adicionarConvidado(String nome, int codigoConvite) {
+        convidadoSet.add(new Convidado(nome, codigoConvite));
+    }
+
+    public void removerConvidadoPorCodigoConvite(int codigoConvite) {
+        Convidado convidadoParaRemover = null;
+        for (Convidado c : convidadoSet) {
+            if (c.getCodigoConvite() == codigoConvite) {
+                convidadoParaRemover = c;
+                break;
+            }
+        }
+        convidadoSet.remove(convidadoParaRemover);
+    }
+
+    public int contarConvidados() {
+        return convidadoSet.size();
+    }
+
+    public void exibirConvidados() {
+        System.out.println(convidadoSet);
+    }
+
+    public static void main(String[] args) {
+        ConjuntoConvidados conjuntoConvidados = new ConjuntoConvidados();
+
+        conjuntoConvidados.adicionarConvidado("Convidado1", 565248);
+        conjuntoConvidados.adicionarConvidado("Convidado2", 565248);
+        conjuntoConvidados.adicionarConvidado("Convidado3", 5658);
+        conjuntoConvidados.adicionarConvidado("Convidado4", 5248);
+        conjuntoConvidados.exibirConvidados();
+
+        conjuntoConvidados.removerConvidadoPorCodigoConvite(565248);
+        conjuntoConvidados.exibirConvidados();
+
+        System.out.println("O número total de convidados é " + conjuntoConvidados.contarConvidados());
+
+
+
+    }
+}
